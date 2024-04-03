fc6cbaef4b97da71ecad2770ddfbd6aa25e3ac79
ThomasKshiju <163325432+ThomasKshiju@users.noreply.github.com>
2024-04-03 23:03:33 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,14 +1,37 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
-
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract DegenToken is ERC20, Ownable {
+contract DegenToken is ERC20, Ownable, ERC20Burnable {
 
-    constructor() ERC20("Degen", "DGN") {}
+    string public redeemShop;
+    uint256[] public itemCosts;
 
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
+    constructor() ERC20("Degen", "DGN") {
+        redeemShop = "There are 5 items in your shop which you are able to redeem: 1. Ball, 2. Cap, 3. Suit, 4. Table, 5. Cloth";
+        itemCosts = [0, 5, 10, 15, 20]; 
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function redeem(uint256 itemNo) public {
+        require(itemNo > 0 && itemNo <= itemCosts.length, "Invalid item selection");
+        require(balanceOf(msg.sender) >= itemCosts[itemNo], "Insufficient balance");
+
+        _burn(msg.sender, itemCosts[itemNo]);
+    }
+
+    function checkBalance(address account) public view returns (uint256) {
+        return balanceOf(account);
+    }
+
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+    function burnAmount(uint256 amount)public {
+        _burn(msg.sender, amount);
     }
 }
